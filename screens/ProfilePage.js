087cfde427cd5847ec40cbafbcf909b005fbf494
Lejla Omerspahic <96488468+lejlaomerspahic@users.noreply.github.com>
2023-07-28 16:23:35 +0200

    image upload

--- screens/ProfilePage.js
+++ screens/ProfilePage.js
@@ -9,77 +9,66 @@ import {
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useUser } from "../hook/useUser";
+import ImagePicker from "react-native-image-picker";
+import styles from "./profile.style";
+
 const ProfilePage = () => {
-  const { user } = useUser();
   const [imageUrl, setImageUrl] = useState("");
 
   const handleImageSubmit = async () => {
-    if (!imageUrl) {
-      console.log("Please enter the image URL");
-      return;
+    ImagePicker.launchImageLibrary({}, (response) => {
+      if (response.uri) {
+        setImageUrl(response.uri); // Postavite URI slike u stanje komponente
+      }
+    });
+  };
+
+  const handleUploadImage = async () => {
+    try {
+      if (!imageUrl) {
+        console.log("Please select an image first");
+        return;
+      }
+
+      // Ovdje šaljete lokalni URI slike na backend
+      const token = await AsyncStorage.getItem("token");
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      axios
+        .post(
+          "http://192.168.0.28:3001/api/products/upload",
+          { imageUrl: imageUrl },
+          config
+        )
+        .then((response) => {
+          console.log("Image URL saved successfully!", response.data);
+        })
+        .catch((error) => {
+          console.error("Error saving image URL:", error.message);
+        });
+    } catch (error) {
+      console.error("Error uploading image:", error.message);
     }
-    const token = await AsyncStorage.getItem("token");
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-    axios
-      .post(
-        "http://192.168.0.28:3001/api/products/upload",
-        { imageUrl },
-        config
-      )
-      .then((response) => {
-        console.log("Image URL saved successfully!", response.data);
-      })
-      .catch((error) => {
-        console.error("Error saving image URL:", error.message);
-      });
   };
 
   return (
     <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter Image URL"
-        onChangeText={(text) => setImageUrl(text)}
-        value={imageUrl}
-      />
+      {imageUrl ? (
+        <Image source={{ uri: imageUrl }} style={styles.image} />
+      ) : null}
+
       <TouchableOpacity style={styles.button} onPress={handleImageSubmit}>
-        <Text style={styles.buttonText}>Save Image URL</Text>
+        <Text style={styles.buttonText}>Choose Image</Text>
       </TouchableOpacity>
-      <Text>{user.user.name}</Text>
+
+      <TouchableOpacity style={styles.button} onPress={handleUploadImage}>
+        <Text style={styles.buttonText}>Upload Image</Text>
+      </TouchableOpacity>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  input: {
-    width: "80%",
-    height: 40,
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 20,
-  },
-  button: {
-    backgroundColor: "blue",
-    paddingHorizontal: 20,
-    paddingVertical: 10,
-    borderRadius: 5,
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
-  },
-});
 
 export default ProfilePage;
---
+++ screens/profile.style.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  input: {
+    width: "80%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "blue",
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default styles;
